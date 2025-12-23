78f395bcd69ae7d888495bd8a3c334f856a5b479
Connor Timmins <connor.timmins@coram.ai>
2025-12-23 14:10:50 +0000

    Refactor to get rid of break label

--- publish_flow_block.go
+++ publish_flow_block.go
@@ -5,80 +5,82 @@ import (
 )
 
 func (publisher *Publisher) startNotifyFlowHandler() {
-	for {
-		select {
-		case <-publisher.done:
-			return
-		default:
-		}
-		if publisher.connManager.IsClosed() {
-			return
-		}
-
-		notifyFlowChan := publisher.chanManager.NotifyFlowSafe(make(chan bool))
+	for publisher.notifyHandlerReady() {
+		flowChan := publisher.chanManager.NotifyFlowSafe(make(chan bool))
 		publisher.disablePublishDueToFlowMu.Lock()
 		publisher.disablePublishDueToFlow = false
 		publisher.disablePublishDueToFlowMu.Unlock()
 
-	flowLoop:
-		for {
-			select {
-			case <-publisher.done:
-				return
-			case ok, open := <-notifyFlowChan:
-				if !open {
-					break flowLoop
-				}
-				publisher.disablePublishDueToFlowMu.Lock()
-				if ok {
-					publisher.options.Logger.Warnf("pausing publishing due to flow request from server")
-					publisher.disablePublishDueToFlow = true
-				} else {
-					publisher.disablePublishDueToFlow = false
-					publisher.options.Logger.Warnf("resuming publishing due to flow request from server")
-				}
-				publisher.disablePublishDueToFlowMu.Unlock()
+		if !publisher.handleFlowEvents(flowChan) {
+			return
+		}
+	}
+}
+
+func (publisher *Publisher) handleFlowEvents(flowChan <-chan bool) bool {
+	for {
+		select {
+		case <-publisher.done:
+			return false
+		case ok, open := <-flowChan:
+			if !open {
+				return true // channel closed, re-register
 			}
+			publisher.disablePublishDueToFlowMu.Lock()
+			if ok {
+				publisher.options.Logger.Warnf("pausing publishing due to flow request from server")
+				publisher.disablePublishDueToFlow = true
+			} else {
+				publisher.disablePublishDueToFlow = false
+				publisher.options.Logger.Warnf("resuming publishing due to flow request from server")
+			}
+			publisher.disablePublishDueToFlowMu.Unlock()
 		}
 	}
 }
 
 func (publisher *Publisher) startNotifyBlockedHandler() {
-	for {
-		// Check if closed before (re-)registering
-		select {
-		case <-publisher.done:
-			return
-		default:
-		}
-		if publisher.connManager.IsClosed() {
-			return
-		}
-
+	for publisher.notifyHandlerReady() {
 		blockings := publisher.connManager.NotifyBlockedSafe(make(chan amqp.Blocking))
 		publisher.disablePublishDueToBlockedMu.Lock()
 		publisher.disablePublishDueToBlocked = false
 		publisher.disablePublishDueToBlockedMu.Unlock()
 
-	blockedLoop:
-		for {
-			select {
-			case <-publisher.done:
-				return
-			case b, open := <-blockings:
-				if !open {
-					break blockedLoop
-				}
-				publisher.disablePublishDueToBlockedMu.Lock()
-				if b.Active {
-					publisher.options.Logger.Warnf("pausing publishing due to TCP blocking from server")
-					publisher.disablePublishDueToBlocked = true
-				} else {
-					publisher.disablePublishDueToBlocked = false
-					publisher.options.Logger.Warnf("resuming publishing due to TCP blocking from server")
-				}
-				publisher.disablePublishDueToBlockedMu.Unlock()
-			}
+		if !publisher.handleBlockedEvents(blockings) {
+			return
 		}
 	}
+}
+
+func (publisher *Publisher) handleBlockedEvents(blockings <-chan amqp.Blocking) bool {
+	for {
+		select {
+		case <-publisher.done:
+			return false
+		case b, open := <-blockings:
+			if !open {
+				return true // channel closed, re-register
+			}
+			publisher.disablePublishDueToBlockedMu.Lock()
+			if b.Active {
+				publisher.options.Logger.Warnf("pausing publishing due to TCP blocking from server")
+				publisher.disablePublishDueToBlocked = true
+			} else {
+				publisher.disablePublishDueToBlocked = false
+				publisher.options.Logger.Warnf("resuming publishing due to TCP blocking from server")
+			}
+			publisher.disablePublishDueToBlockedMu.Unlock()
+		}
+	}
+}
+
+// notifyHandlerReady checks if the handler should proceed to register.
+// Returns false if publisher is closing or connection is closed.
+func (publisher *Publisher) notifyHandlerReady() bool {
+	select {
+	case <-publisher.done:
+		return false
+	default:
+	}
+	return !publisher.connManager.IsClosed()
 }
